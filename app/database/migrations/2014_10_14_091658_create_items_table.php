b32739241db9a965c53960c84172a5cc7f84fa88
TKimura416 <kimuratakuya416@gmail.com>
2014-10-15 07:04:21 +0800

    up

--- app/database/migrations/2014_10_14_091658_create_items_table.php
+++ app/database/migrations/2014_10_14_091658_create_items_table.php
@@ -15,7 +15,23 @@ class CreateItemsTable extends Migration {
 		Schema::create('items', function(Blueprint $table)
 		{
 			$table->increments('id');
-                        $table->string('tag', 10);
+                        $table->string('tag', 10)
+                              ->unique();
+                        $table->char('depcode', 2);
+                        $table->char('itemcode', 3);
+                        $table->string('oldcode', 30);
+                        $table->string('newcode', 30);
+                        $table->string('brand', 50);
+                        $table->string('model', 20);
+                        $table->string('serial', 30);
+                        $table->text('description');
+                        $table->string('quantity', 10);
+                        $table->text('documents');
+                        $table->date('purchased');
+                        $table->string('deliveryrcpt', 10);
+                        $table->string('invoicercpt', 10);
+                        $table->string('nameunder', 10);
+                        $table->string('reference', 10);
 			$table->timestamps();
 		});
 	}
