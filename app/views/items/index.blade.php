d2368a40356f9f2ca1979194f9c78c72e48588fa
TKimura416 <kimuratakuya416@gmail.com>
2014-10-25 10:17:55 +0800

    up

--- app/views/items/index.blade.php
+++ app/views/items/index.blade.php
@@ -1,38 +1,4 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Ribbit - A Treehouse Project</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="../../assets/js/html5shiv.js"></script>
-      <script src="../../assets/js/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <div>
-      <header>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About Ribbit</a></li>
-          <li><a href="#">Treehouse</a></li>
-        </ul>
-        <h3>Ribbit</h3>
-      </header>
-      
-      <div>
-        <h1>Self-destructing message app</h1>
-        <p>Learn how to build this fun little app by signing up for a Treehouse account today! We'll teach you how to build both apps from scratch!</p>
-        <p>
-          <a href="#">Download the app</a>
-          <a href="#" >Text me the link</a>
-        </p> 
-      </div>
-
-      <div>
-        <p>&copy; 2013 Ribbit - By Treehouse Island, Inc.</p>
-      </div>
-    </div>
-  </body>
-</html>
+@extends('layouts.main')
+@section('content')
+<h1>Hi</h1>
+@stop
